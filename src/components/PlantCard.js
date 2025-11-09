f4400eac52bfb2eb9e334140b78bdbcb5ec3cb1c
njoroofficial <mosesn579@gmail.com>
2025-11-09 09:39:42 +0300

    Implemented functionality to mark plants as sold out

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({ name, image, price }) {
+  const [inStock, setIsStock] = useState(true);
+
+  function handleClick() {
+    setIsStock((prevState) => !prevState);
+  }
+
   return (
     <li className="card" data-testid="plant-item">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+
+      <button className="primary" onClick={handleClick}>
+        {inStock ? "In Stock" : "Out of Stock"}
+      </button>
     </li>
   );
 }
